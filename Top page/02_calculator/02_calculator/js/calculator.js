9748146e09f3ae312b7c10e8ab5890b12872354b
aman707422 <au72271@gmail.com>
2026-01-23 14:46:01 +0900

    Add files via upload

---
+++ Top page/02_calculator/02_calculator/js/calculator.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const elementSelect = document.getElementById("calcType");
+const elementNum1 = document.getElementById("num1");
+const elementnum2 = document.getElementById("num2");
+const elementResult = document.getElementById("result");
+const elementbtnEqual = document.getElementById("btnEqual");
+
+elementSelect.addEventListener("change", clear);
+elementNum1.addEventListener("change", clear);
+elementnum2.addEventListener("change", clear);
+
+elementbtnEqual.addEventListener("click", update);
+
+function update() {
+    const result = calculate(
+        Number(elementNum1.value),
+        Number(elementnum2.value),
+        elementSelect.value
+    );
+    elementResult.innerHTML = result;
+}
+
+function calculate(num1, num2, calcType) {
+    let result;
+
+    switch (calcType) {
+        case "type-add":
+            result = num1 + num2;
+            break;
+        case "type-substract":
+            result = num1 - num2;
+            break;
+        case "type-multiply":
+            result = num1 * num2;
+            break;
+        case "type-divide":
+            result = num1 / num2;
+            break;
+    }
+    return result;
+}
+function clear() {
+    elementResult.innerHTML = "";
+}
